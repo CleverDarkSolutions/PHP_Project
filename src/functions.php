0150f5ef684d0e9699e359c0146253affd790b71
CleverDarkSolutions <66365110+CleverDarkSolutions@users.noreply.github.com>
2021-05-29 19:09:18 +0200

    pare funkcji

--- src/functions.php
+++ src/functions.php
@@ -1,7 +1,6 @@
 <?php 
 
-function fetchProducts($con){
-    
+function fetchProducts($con){ // for everyone
     $query = 'SELECT * FROM PRODUKT';
 
     echo "<div class=productsCombined>";
@@ -16,6 +15,52 @@ function fetchProducts($con){
         
         echo "</div>";
     }
+    echo "</div>";
+}
+
+function login($username, $password, $con){
+    if( isset($_SESSION['login']) && isset($_SESSION['password']) )
+        return 0; 
+    else {
+        $loginquery = 'SELECT nazwa, haslo FROM user';
+        while($row = mysqli_fetch_assoc($con,$loginquery)){
+            if($row['nazwa'] == $username && $row['haslo'] == $password){
+                $_SESSION['login'] = $username;
+                $_SESSION['password'] = $password;
+            }
+        }
+    }
+}
+
+function logoff(){ // to be expanded
+    $_SESSION[] = [];
+    header('Location: index.php');
+}
+
+function cart(){
+    $items = $_SESSION['items'];
+    echo "<table class=table>";
+    echo "<thead>";
+    echo "<tr>";
+    echo "<th scope=col>ID</th>";
+    echo "<th scope=col>Name</th>";
+    echo "<th scope=col>Price</th>";
+    echo "<th scope=col>Quantity</th>";
+    echo "<th scope=col>Size</th>";
+    echo "<th scope=col>Image</th>";
+    echo "</tr> </thead>";
+
+    for($i=0;$i<count($items);$i++){
+        echo "<tr>";
+        echo "<th scope=row>".$items[$i]['id']."</th>";
+        echo "<td>".$items[$i]['name']."</td>";
+        echo "<td>".$items[$i]['price']."</td>";
+        echo "<td>".$items[$i]['quantity']."</td>";
+        echo "<td>".$items[$i]['size']."</td>";
+        echo "<td><img src=".$items[$i]['link']."> </td>";
+        echo "</tr>";
+    }
+    echo "</table>"; 
 }
 
 ?>
