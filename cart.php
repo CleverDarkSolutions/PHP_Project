819d99d171f15a895c75a0aeb53422811b8fdd27
CleverDarkSolutions <66365110+CleverDarkSolutions@users.noreply.github.com>
2021-06-02 14:28:37 +0200

    sjkaasld

--- cart.php
+++ cart.php
@@ -1,7 +1,7 @@
 <?php
 include 'functions.php';
 $ //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("mysql://b8037eebd25c3c:a17473d7@eu-cdbr-west-01.cleardb.com/heroku_f5b528b5400594f?reconnect=true"));
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
